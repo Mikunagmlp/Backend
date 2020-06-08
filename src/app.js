046a5cfbf04c991351e5fbf017654e6a9d55d1be
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-06-08 14:48:02 -0400

    Utilizando CORS para hacer pruebas con angular

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 require('./db/mongoose'); // importa la DB
 const usuarioRouter = require('./routes/usuario');
 
+const cors = require('cors');
 const app = express();
 
 // hace el parse de data enviada del cliente en json se convierta en objeto para nuestro server
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 // registramos nuestro router para poder usarlo
 app.use(usuarioRouter);
