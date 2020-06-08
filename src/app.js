f91b2ff9b3528435aeecc5e6da3f3b8ecb35f900
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-06-08 18:56:17 -0400

    Insertando en app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const express = require('express');
 require('./db/mongoose'); // importa la DB
 const usuarioRouter = require('./routes/usuario');
 const entidadRouter = require('./routes/entidad');
+const administracionRouter= require('./routes/administracion');
+const almacenRouter= require('./routes/almacen');
+const categoriaRouter = require('./routes/categoria');
+const colegioRouter=require('./routes/colegio');
+const posicionRouter=require('./routes/posicion');
+const productoRouter= require('./routes/producto');
+const proveedorRouter=require('./routes/proveedor');
 
 const cors = require('cors');
 const app = express();
@@ -14,5 +21,12 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 // registramos nuestro router para poder usarlo
 app.use(usuarioRouter);
 app.use(entidadRouter);
+app.use(administracionRouter);
+app.use(almacenRouter);
+app.use(categoriaRouter);
+app.use(colegioRouter);
+app.use(posicionRouter);
+app.use(productoRouter);
+app.use(proveedorRouter);
 
 module.exports = app;
