b7b74f3a49fe247e7a54443b2148cf3022b99ba8
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-06-15 15:40:29 -0400

    creacion de roles y permisos

--- src/routes/usuario/usuario.js
+++ src/routes/usuario/usuario.js
@@ -3,7 +3,37 @@ const express = require('express');
 const router = new express.Router();
 
 const User = require('../../models/usuario');
+// token
+const bcrypt = require('bcrypt')
 
+/* 
+Nueva entrada
+async function hashPassword(password){
+    return await bcrypt.hash(Password ,10);
+}
+async function validatePassword(plainPassword, hashedPassword){
+    return await bcrypt.compare(plainPassword, hashedPassword);
+}
+    exports.signup = async (req,res,next) ={
+        try{1
+            const {NombreCompleto, Email, Role, accestoken, Password, Telefono, Direccion, Genero, Estado} = req.body
+            const hashedPassword = await hashPassword(password);
+            const newUSer = new User({Email, Role,Password: hashedPassword, role:role || "admi" });
+            const accesToken = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET,{expiresIn:"1d"});
+            newUSer.accesToken = accessToken;
+            await newUser.save();
+            res.json({
+                data:newUSer,
+                accesToken
+            })
+
+        } catch (error){
+            next(error)
+        }
+    }
+
+
+*/
 // Login Usuario
 router.post('/user/login', async (req, res) => {
     const email = req.body.Email;
