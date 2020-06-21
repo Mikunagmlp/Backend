e3f9d96e6e75a4cacc48fce2793bb0bc45f9e144
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-06-21 18:08:50 -0400

    test de envio email y password

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const jwt = require('jsonwebtoken');
 const User = require('../models/usuario');
 
 passport.serializeUser((user, done) => {
@@ -19,7 +20,7 @@ passport.use('local-signin', new LocalStrategy({
     if (!user) {
         return done(null, false, req.flash('signinMessage', 'No user Found'));
     }
-  
+
     if (!user.comparePassword(Password)) {
         return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
     }
@@ -27,7 +28,11 @@ passport.use('local-signin', new LocalStrategy({
     return done(null, user);
 }));
 
-exports.isAuthenticated = (req, res, next)=> {
+exports.login = (req, res) => {
+    console.log(req.body);
+}
+
+exports.isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const passportConfig = require('../passport/local-auth');
+const { isLogin } = require('../passport/localAuth');
 router.get('/', (req, res, next) => {
     res.send('Index');
 });
 
-router.post('/signin', passport.authenticate('local-signin', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/signin',
-    passReqToCallback: true
-}));
+router.post('/signin', isLogin);
 
 router.get('/logout', (req, res, next) => {
     req.logout();
