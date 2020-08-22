4efe7dc19649fd4a36427fc7bda0f559f1992ee0
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-08-22 19:26:33 -0400

    envio de menu a eba y unace

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = new express.Router();
-const { createMenu,getSolidoInicial, getLiquidoInicial,getSolidoPrimario, getLiquidoPrimario,getSolidoSegundario,getLiquidoSegundario } = require('../controllers/menu.controller');
+const { createMenu,getSolidoInicial, getLiquidoInicial,getSolidoPrimario, getLiquidoPrimario,getSolidoSegundario,getLiquidoSegundario,updateMenu,listarMenuAprobado,listarMenuNoAprobado,listarMenuEba,listarMenuUnace,aprobarMenuEba, aprobarMenuUnace } = require('../controllers/menu.controller');
 
 router.post('/menu/registrar/menudiario', createMenu);
+router.patch('/menu/update/menudiario/:id', updateMenu);
+
+router.get('/menu/listado/aprobado', listarMenuAprobado);
+router.get('/menu/listado/noaprobado', listarMenuNoAprobado);
+router.get('/menu/listado/eba', listarMenuEba);
+router.get('/menu/listado/unace', listarMenuUnace);
+
+router.patch('/menu/aprobar/menueba/:id', aprobarMenuEba);
+router.patch('/menu/aprobar/menuunace/:id', aprobarMenuUnace);
 
 router.get('/menu/productos/getSolidoInicial', getSolidoInicial);
 router.get('/menu/productos/getLiquidoInicial', getLiquidoInicial);
