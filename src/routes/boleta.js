1c7e07ed5ac65a2abec2bbdcb52ff52048ac5bef
Christian <christiantusco@gmail.com>
2020-08-22 14:16:48 -0400

    boleta1

---
+++ src/routes/boleta.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = new express.Router();
+const { createBoleta, getBoleta, getBoleta, updateBoleta } = require('.//../controllers/boleta.controller');
+
+router.post('/boleta/registrar', createBoleta);
+
+router.get('/boleta', getBoleta);
+
+router.get('/boleta/:id', getBoleta);
+
+router.patch('/boleta/editar/:id', updateBoleta);
+
+
+
+module.exports = router;
