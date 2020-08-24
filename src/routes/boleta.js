7345279f40e677d8d5d14cded3cb957c45e83c02
joseMendoza9696 <jmendozacarrasco@gmail.com>
2020-08-23 22:13:47 -0400

    Bugs

--- src/routes/boleta.js
+++ src/routes/boleta.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = new express.Router();
-const { createBoleta, getBoleta, getBoleta, updateBoleta } = require('.//../controllers/boleta.controller');
-
-router.post('/boleta/registrar', createBoleta);
-
-router.get('/boleta', getBoleta);
-
-router.get('/boleta/:id', getBoleta);
-
-router.patch('/boleta/editar/:id', updateBoleta);
+// const { createBoleta, getBoleta, updateBoleta } = require('.//../controllers/boleta.controller');
+//
+// router.post('/boleta/registrar', createBoleta);
+//
+// router.get('/boleta', getBoleta);
+//
+// router.get('/boleta/:id', getBoleta);
+//
+// router.patch('/boleta/editar/:id', updateBoleta);
 
 
 
