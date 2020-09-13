b8e1349c2464c8798e47d4440764a071ae189ede
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-09-13 10:43:36 -0400

    Boleta update firma

--- src/routes/boleta.js
+++ src/routes/boleta.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = new express.Router();
 
-const { listaAsignaciones, crearBoleta,listaBoletas,listaCodigoActa, firmaColegio, firmaEBA, firmaSiremu } = require('.//../controllers/boleta.controller');
+const { listaAsignaciones, crearBoleta, listaBoletas, listaCodigoActa, firmaColegio, firmaEBA, firmaSiremu, firmaBoleta } = require('.//../controllers/boleta.controller');
 
 router.get('/boleta/listado/asigaciones', listaAsignaciones);
 router.post('/boleta/registrar/:id', crearBoleta);
 router.get('/boleta/listado/allboletas', listaBoletas);
 router.get('/boleta/listado/codigoacta', listaCodigoActa);
+router.post('/boleta/firma/:id', firmaBoleta);
 
 router.post('/boleta/aplicacion-colegio', firmaColegio);
 router.post('/boleta/aplicacion-eba', firmaEBA);
